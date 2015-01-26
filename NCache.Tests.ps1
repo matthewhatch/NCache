284180543982432a0b3ddb156fd2daf6bf9f44e1
matthewhatch <matthew.hatch@fmglobal.com>
2015-01-26 11:18:42 -0500

    Initial Commit

---
+++ NCache.Tests.ps1
@@ -0,0 +1,59 @@
+﻿Import-Module ./NCache.psm1 -Force
+$cred = Get-Credential
+
+Describe 'Get-CacheDetails' {
+    $Cache = Get-CacheDetails -ComputerName 'johncachd01' -CacheID 'extranetcache' -Credential $cred
+    $params = (Get-Command Get-CacheDetails).Parameters
+
+    It 'accepts Credential as a parameter'{
+        $params.ContainsKey('Credential') | Should Be $true
+    }
+
+    It 'returns an object' {
+        $Cache.GetType().Name | Should Be 'PSCustomObject'
+    }
+
+    It 'returns object with cache id extranetcache' {
+        $Cache.CacheID | Should Be 'ExtranetCache'   
+    }
+
+    It 'returns a int as cluster size' {
+        ($Cache.ClusterSize -as [System.Int16]).GetType().Name | Should Be 'Int16'
+    }
+
+    It 'returns extranetcache as the cacheID' {
+        $Cache.CacheID | Should Be 'extranetcache'
+    }
+
+    It 'returns johncachd01 as Server' {
+        $Cache.ComputerName | Should Be 'johncachd01'
+    }
+    
+    It 'returns an object with property CacheID' {
+        (Get-Member -InputObject $Cache).Name -contains 'CacheID' | Should Be $true
+    } 
+
+    It 'returns an object with property ComputerName' {
+        (Get-Member -InputObject $Cache).Name -contains 'ComputerName' | Should Be $true
+    } 
+
+    It 'returns an object with property Count' {
+        (Get-Member -InputObject $Cache).Name -contains 'Count' | Should Be $true
+    } 
+
+    It 'returns an object with property ClusterSize' {
+        (Get-Member -InputObject $Cache).Name -contains 'ClusterSize' | Should Be $true
+    } 
+
+    It 'returns an object with property Uptime' {
+        (Get-Member -InputObject $Cache).Name -contains 'Uptime' | Should Be $true
+    } 
+
+    It 'returns an object with property Status' {
+        (Get-Member -InputObject $Cache).Name -contains 'Status' | Should Be $true
+    } 
+
+    It 'returns a status of Running or Stopped' {
+        $Cache.Status -eq 'Running' -or $Cache.Status -eq 'Stopped' | Should Be $true
+    }
+}
---
+++ NCache.psm1
@@ -0,0 +1,145 @@
+﻿function Get-CacheDetails{
+    
+    <#
+        .Synopsis
+        Returns the detailed information about ncache cache
+
+        .Description
+        Returns detailed information about an ncache cache that is passed to the
+        CacheID parameter
+
+        .Parameter ComputerName
+        Name of the server to retreive cache information from
+
+        .Parameter CacheID
+        Name of the Cache that to return the details of
+            
+    #>
+    [CmdletBinding()]
+    param(
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+
+        [Parameter(Mandatory=$true)]
+        [string[]]$CacheID,
+
+        [PSCredential]$Credential
+    )
+
+    BEGIN{
+        $CacheDetailsBlock = {
+            $results = & listcaches /a
+            Write-Output $results
+        }
+
+        function __validateCacheResults{
+            param([PSObject]$CacheResults)
+        
+            $isValid = $true
+            do{
+                if($CacheResults.CacheID -ne $CacheID){
+                    Write-Verbose "CacheID $CacheID is not Valid"
+                    $isValid = $false
+                    break
+                }
+
+                if($CacheResults.Status -ne 'Running'){
+                    Write-Verbose "Status $($CacheResults.Status) is not Valid"
+                    $isValid = $false
+                    break
+                }
+
+                if($CacheResults.Capacity -notmatch 'MB'){
+                    Write-Verbose "Cluster Capacity$($CacheResults.Capacity) is not valid"
+                    $isValid =$false 
+                    break
+                }
+
+                if($CacheResults.Clustersize -notmatch '\d+'){
+                    Write-Verbose "Cluster Size $($CacheResults.ClusterSize) is not valid"   
+                    $isValid = $false
+                    break
+                }
+
+                if($CacheResults.Count -notmatch '\d+'){
+                    Write-Verbose "Count $($CacheResults.Count) is not valid"
+                    $isValid = $false
+                    break
+                }
+
+            }
+            while($false)
+            return $isValid
+        }
+    
+    }
+    PROCESS{
+        foreach($Computer in $ComputerName){
+            
+            if($Computer -eq $env:COMPUTERNAME -or $Computer -eq '.'){
+                try{$CacheDetails = & listcaches /a}
+                catch{ Write-Warning "there was an issue retrieving $CacheID Details from $Computer"}    
+            }
+            else{
+                try{
+                    $cimParameters = @{
+                        ComputerName = $Computer
+                        ScriptBlock = $CacheDetailsBlock
+                    }
+
+                    if($PSBoundParameters.ContainsKey('Credential')){
+                        $cimParameters.Add('Credential',$Credential)
+                    }
+
+                    $CacheDetails = Invoke-Command @cimParameters -ErrorAction Stop
+                }
+                catch{
+                    Write-Warning "there was an issue retrieving $CacheID Details from $Computer"
+                }
+            }
+            if($CacheDetails){
+                
+                foreach($cache in $CacheID){
+                    $StartIndex = __get-cacheStartIndex -CacheList $CacheDetails -CacheID $cache
+                    Write-Verbose "The start index is $StartIndex"
+        
+                    $Clustersize = $CacheDetails[$StartIndex + 3].Replace('Cluster size:','').TrimStart()
+                    $properties = @{
+                        ComputerName = $Computer
+                        CacheId = $CacheDetails[$StartIndex].Replace('Cache-ID:','').TrimStart()
+                        Status = $CacheDetails[$StartIndex + 2].Replace('Status:','').TrimStart()
+                        ClusterSize = $Clustersize
+                        Uptime = $CacheDetails[$StartIndex + (4 + $Clustersize)].Replace('UpTime:','').TrimStart()
+                        Capacity = $CacheDetails[$StartIndex + (5 +$Clustersize)].Replace('Capacity:','').TrimStart()
+                        Count = $CacheDetails[$StartIndex + (6 + $Clustersize)].Replace('Count:','').TrimStart()
+                    }
+            
+                    $detailsObject = New-Object -TypeName PSObject -Property $properties
+            
+                    Remove-Variable -Name properties
+                    Remove-Variable -Name CacheDetails
+            
+                    if((__ValidateCacheResults $detailsObject)){Write-Output $detailsObject}
+                }
+
+            }     
+        }
+    }
+    END{}
+   
+}
+
+function __get-CacheStartIndex{
+    param($Cachelist,$CacheID)
+
+    #Find the line where cacheid matches the cache if passed
+    $i = 0 #start of the array
+    while($i -lt ($Cachelist.Length -1)){
+        if($Cachelist[$i] -match $CacheID){
+            return $i
+            break
+        }
+        $i++
+    }
+}
+
+Export-ModuleMember -Function Get-CacheDetails
